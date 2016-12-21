37f2391ecb102b7e0d88a838273b5f0301cce511
pawel <paw94@op.pl>
2016-12-21 15:43:31 +0100

    WIN zad1

--- patrwin2017/ConsoleApp1/src/ConsoleApp1/Properties/AssemblyInfo.cs
+++ patrwin2017/ConsoleApp1/src/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("patronat")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("20a96b54-6ecc-48ae-b646-88fdc18dec8b")]
+[assembly: Guid("dc7b3746-d055-45bb-99c3-2fb9f59cbc96")]
